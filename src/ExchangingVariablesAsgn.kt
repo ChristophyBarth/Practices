5e26c497d89939e273c224b94b95b2d4f8538388
chrisbarth01 <christopherbartholomewokeke@gmail.com>
2020-06-15 13:33:54 +0100

    Exchanging Variables commit

---
+++ src/ExchangingVariablesAsgn.kt
@@ -0,0 +1,26 @@
+fun main (){
+    var victor : String = "Football"
+    var anna : String = "Teddy Bear"
+    var dad : String = " "
+
+    println("Victor has a football while anna has a teddy bear")
+    println("Hey, I'm Victor and I have a $victor")
+    println("Hey, I'm Anna and I have a $anna")
+    println()
+
+
+    dad = victor
+    victor = anna
+    anna = dad
+
+    println("But one day, their dad decided to swap their toys")
+    println("Hey, I'm Victor and I now have a $victor")
+    println("Hey, I'm Anna and I now have a $anna")
+
+
+
+
+
+
+
+}

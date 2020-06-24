9a7db6088d6ba594ccd18b4b8e113bfc63350d28
chrisbarth01 <christopherbartholomewokeke@gmail.com>
2020-06-24 12:37:33 +0100

    Practices

--- src/Square calculator.kt
+++ src/Square calculator.kt
@@ -1,5 +1,22 @@
 fun main (){
-    val num = readLine()!!.toInt()
+    val num = readLine()!!.toDouble()
     print("The Square of $num is ")
     println(num * num)
+
+    println("Tell me the radius of a circle and I'll tell you its circumference")
+    val input = readLine()!!.toDouble()
+    println("Was that in centimeter or Meter\nInput 1 for CM\nInput 2 for M")
+    val selection = readLine()!!.toDouble()
+
+
+    if (selection == 1.0){
+        println("The circumference is ${2 * 3.1428 * input}CM")
+
+    }
+
+    if (selection == 2.0){
+        val unit = input/100
+        println("The circumference is ${2 * 3.1428 * unit}M")
+
+    }
 }
